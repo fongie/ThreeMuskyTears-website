25cd9d6d325858deb28a562824f17bc139d69b49
Max Körlinge <mkorlinge@hotmail.com>
2016-08-22 21:11:49 +0200

    First commit!!

---
+++ editproj.bat
@@ -0,0 +1,7 @@
+@echo off
+
+start notepad++.exe index.html
+start notepad++.exe contact.html
+start notepad++.exe tour.html
+start notepad++.exe css/main.css
+start notepad++.exe css/responsive.css
